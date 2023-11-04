1a3838bc26f8c4bf05fd8ee29726c034e5560293
Patrick <patbied@gmail.com>
2023-11-04 14:37:29 -0400

    Added react router and one path for home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Navbar from './components/navbar'
+import { useRoutes } from 'react-router-dom'
+import Home from './pages/Home'
 function App() {
-  
-
+  let subpages = useRoutes([
+    {
+      path: '/',
+      element: <Home/>
+    }
+  ])
   return (
     <>
       <Navbar/>
+      {subpages}
     </>
   )
 }
