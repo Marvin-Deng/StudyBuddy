1b0d8409ce1f465982b680f5b2fe340e043da772
Patrick <patbied@gmail.com>
2023-11-04 14:29:55 -0400

    Cleaned up default CSS, added PicoCSS and implemented a basic navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '@picocss/pico'
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <li><strong>TutorMe</strong></li>
+        </ul>
+        <ul>
+            <li><a href="#" role="button">Login</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
