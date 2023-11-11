96e4ccb5e277903117d9ae71d10cf5276f483bfa
Marvin <mdengomg2@gmail.com>
2023-11-11 15:41:39 -0800

    Added an endpoint to keyword search for students

--- server/controllers/StudentController.js
+++ server/controllers/StudentController.js
@@ -1,65 +1,93 @@
-import { pool } from '../config/db.js'
+import { pool } from "../config/db.js";
 
 class StudentController {
-
-    static async getOne(id) {
-        try {
-            const results = await pool.query('SELECT * FROM students WHERE id = $1',[id])
-            return results.rows
-        }
-        catch (error) {
-            return { success: false, message: 'An error occurred: ' + error.message }
-        }
+  static async getOne(id) {
+    try {
+      const results = await pool.query("SELECT * FROM students WHERE id = $1", [
+        id,
+      ]);
+      return results.rows;
+    } catch (error) {
+      return { success: false, message: "An error occurred: " + error.message };
     }
-    static async getAllStudents() {
-        try {
-            const results = await pool.query('SELECT * FROM students')
-            return results.rows
-        }
-        catch (error) {
-            return { success: false, message: 'An error occurred: ' + error.message }
-        }
-    }
+  }
 
-    static async joinStudyGroup(studentId, groupId) {
-        try {
-            const query = `
+  static async getAllStudents() {
+    try {
+      const results = await pool.query("SELECT * FROM students");
+      return results.rows;
+    } catch (error) {
+      return { success: false, message: "An error occurred: " + error.message };
+    }
+  }
+
+  static async filterStudents(searchString) {
+    try {
+      const query = `
+      SELECT DISTINCT students.*
+      FROM students
+      WHERE
+          (
+              SELECT COUNT(*)
+              FROM unnest(string_to_array($1, ' ')) AS word
+              WHERE
+                  to_tsvector('english', students.name) @@ to_tsquery('english', word)
+                  OR to_tsvector('english', students.school) @@ to_tsquery('english', word)
+                  OR to_tsvector('english', students.major) @@ to_tsquery('english', word)
+                  OR students.id IN (
+                      SELECT student_id
+                      FROM student_classes
+                      WHERE class_id IN (
+                          SELECT id
+                          FROM classes
+                          WHERE to_tsvector('english', name) @@ to_tsquery('english', word)
+                          OR to_tsvector('english', subject) @@ to_tsquery('english', word)
+                      )
+                  )
+          ) > 0;
+        `;
+      const results = await pool.query(query, [searchString]);
+      return results.rows;
+    } catch (error) {
+      return { success: false, message: "An error occurred: " + error.message };
+    }
+  }
+
+  static async joinStudyGroup(student_id, group_id) {
+    try {
+      const query = `
                 INSERT INTO student_study_groups (student_id, group_id)
                 VALUES ($1, $2);
-            `
-            const result = await pool.query(query, [studentId, groupId])
+            `;
+      const result = await pool.query(query, [student_id, group_id]);
 
-            if (result.rowCount === 1) {
-                return { success: true, message: 'Student joined the study group.' }
-            }
-            else {
-                return { success: false, message: 'Failed to join the study group.' }
-            }
-        }
-        catch (error) {
-            return { success: false, message: 'An error occurred: ' + error.message }
-        }
+      if (result.rowCount === 1) {
+        return { success: true, message: "Student joined the study group." };
+      } else {
+        return { success: false, message: "Failed to join the study group." };
+      }
+    } catch (error) {
+      return { success: false, message: "An error occurred: " + error.message };
     }
+  }
 
-    static async leaveStudyGroup(studentId, groupId) {
-        try {
-            const query = `
+  static async leaveStudyGroup(student_id, group_id) {
+    try {
+      const query = `
                 DELETE FROM student_study_groups
                 WHERE student_id = $1 AND group_id = $2
-            `
-            const result = await pool.query(query, [studentId, groupId])
+            `;
+      const result = await pool.query(query, [student_id, group_id]);
 
-            if (result.rowCount === 1) {
-                return { success: true, message: 'Student left the study group.' }
-            }
-            else {
-                return { success: false, message: 'Failed to leave the study group.' }
-            }
-        }
-        catch (error) {
-            return { success: false, message: 'An error occurred: ' + error.message }
-        }
+      if (result.rowCount === 1) {
+        return { success: true, message: "Student left the study group." };
+      } else {
+        return { success: false, message: "Failed to leave the study group." };
+      }
+    } catch (error) {
+      return { success: false, message: "An error occurred: " + error.message };
     }
+  }
 }
 
-export default StudentController
+export default StudentController;
--- server/routes/StudentRoute.js
+++ server/routes/StudentRoute.js
@@ -4,6 +4,7 @@ import StudentView from '../views/StudentView.js'
 const router = express.Router()
 
 router.get('/getAll', StudentView.getAllStudents)
+router.get('/search/:search_string', StudentView.filterStudents)
 router.get('/:student_id',StudentView.getOne)
 router.post('/joinGroup', StudentView.joinStudyGroup)
 router.delete('/leaveGroup', StudentView.leaveStudyGroup)
