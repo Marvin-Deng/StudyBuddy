b99ef8775bb9fadabb4bf61d7db71f0b1f060c76
Patrick <patbied@gmail.com>
2023-11-11 13:25:43 -0500

    removed console.log

--- server/views/ClassView.js
+++ server/views/ClassView.js
@@ -2,7 +2,7 @@ import ClassController from "../controllers/ClassController.js";
 
 class ClassView{
     static async createClass(req, res){
-        
+
         try {
             const {name, subject, professor} = req.body
             if (!name || !subject || !professor) {
@@ -10,7 +10,6 @@ class ClassView{
             }
 
             const results = await ClassController.createClass(name,subject,professor)
-            console.log(results)
             res.status(200).json(results)
 
         } catch (error) {
