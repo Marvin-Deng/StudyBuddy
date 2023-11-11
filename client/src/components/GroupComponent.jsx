b769959d7425ac2fae3159cb38116012192b9258
Patrick <patbied@gmail.com>
2023-11-11 14:50:13 -0500

    Class info is now shown on  study group page

--- client/src/components/GroupComponent.jsx
+++ client/src/components/GroupComponent.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
 import { Card, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
-const GroupComponent = ({ id, name, description, location, time }) => {
+const GroupComponent = ({ id, name, description, location, time,class_id }) => {
+  const [class_,setClass_] = useState({})
+  useEffect(() => {
+    const getClassByID = async() => {
+      const response = await fetch(`http://localhost:3001/class/${class_id}`)
+      const data = await response.json()
+      // console.log(data[0])
+      setClass_(data[0])
+    }
+    getClassByID()
+  },[])
   return (
     <Card className="display-card">
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>
+          <span className="mb-3">
+           {class_ && class_.name} with professor {class_ && class_.professor}
+          </span>
+          <br/>
           <span className="mb-3">
             {location} - {time}
           </span>
