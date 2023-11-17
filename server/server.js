3475fe0a771f6e52d808a8ac3461a804e48f0a21
Marvin <mdengomg2@gmail.com>
2023-11-17 12:12:08 -0800

    Updated server,js with the deployed client url

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.json())
 // }))
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: process.env.APP_URL || 'http://localhost:5173',
     methods: 'GET,POST,PUT,DELETE,PATCH',
     credentials: true
 }))
