dd9d22b11c81aed49d8ce11c8090f3716b2359dd
Marvin <mdengomg2@gmail.com>
2023-11-17 15:51:30 -0800

    Removed auth comments

--- server/server.js
+++ server/server.js
@@ -1,10 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
-import passport from 'passport'
-import session from 'express-session'
-// import { Github } from './config/auth.js'
-// import AuthRoute from './routes/AuthRoute.js'
 import StudentRoute from './routes/StudentRoute.js'
 import GroupRoute from './routes/GroupRoute.js'
 import ClassRoute from './routes/ClassRoute.js'
@@ -13,31 +9,12 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 
-// app.use(session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false
-// }))
-
 app.use(cors({
     origin: process.env.APP_URL || 'http://localhost:5173',
     methods: 'GET,POST,PUT,DELETE,PATCH',
     credentials: true
 }))
 
-// app.use(passport.initialize())
-// app.use(passport.session())
-
-// passport.use(Github)
-
-// passport.serializeUser((user, done) => {
-//     done(null, user)
-// })
-
-// passport.deserializeUser((user, done) => {
-//     done(null, user)
-// })
-
-// app.use('/auth', AuthRoute)
 app.use('/student', StudentRoute)
 app.use('/group', GroupRoute)
 app.use('/class', ClassRoute)
